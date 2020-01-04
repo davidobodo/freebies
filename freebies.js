dcb7e8deae26fba3766da1d920da26d04907aded
David Obodo <obododavid5@gmail.com>
2020-01-04 14:57:50 +0100

    cartesian implementation for distance from mid point of element complete

--- freebies.js
+++ freebies.js
@@ -1,6 +1,11 @@
 const firstElement = document.querySelector('.img:nth-child(1)')
-firstElement.addEventListener('mousemove', e => console.log(e))
+firstElement.addEventListener('click', move)
 
 function move(e) {
-    console.log(e)
+    var rect = e.target.getBoundingClientRect();
+    var width = rect.width;
+    var height = rect.height;
+    var positionX = e.clientX - (width / 2);
+    var positionY = e.clientY - (height / 2);
+    console.log(positionX, positionY)
 }
