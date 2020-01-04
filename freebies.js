753ccc5d61f0df8f0e37bddaeb6ff0315aaad66f
David Obodo <obododavid5@gmail.com>
2020-01-04 22:30:46 +0100

    change all elements position

--- freebies.js
+++ freebies.js
@@ -1,4 +1,6 @@
 const firstElement = document.querySelector('.img:nth-child(1)')
+const allElements = document.querySelectorAll('.img');
+console.log(allElements)
 firstElement.addEventListener('mousemove', move)
 
 function move(e) {
@@ -7,5 +9,7 @@ function move(e) {
     var height = rect.height;
     var positionX = e.clientX - (width / 2);
     var positionY = e.clientY - (height / 2);
-    firstElement.style.transform = `translate3d(${positionX}px, ${positionY}px, 0)`;
+    var actualX = (positionX / width) * 15
+    var actualY = (positionY / width) * 15
+    allElements.forEach(e => e.style.transform = `translate3d(${actualX}px, ${actualY}px, 0)`)
 }
